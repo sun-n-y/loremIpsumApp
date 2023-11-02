7ff9362c2932292ee0e07bd4a52120b2fa900dab
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-02 10:46:34 -0400

    add inital setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
+import { useState } from 'react';
+import data from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min={'1'}
+          step={'1'}
+          max={'8'}
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+    </section>
+  );
 };
 export default App;
